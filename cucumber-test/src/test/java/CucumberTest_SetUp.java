ae415c28eb04d13de070ca0a824a8e5c01d89e89
David Miles-Skov <davidmilesskov@protonmail.com>
2022-03-18 15:13:59 +0100

    Minor changes to pom.xml and cucumbertest

--- cucumber-test/src/test/java/CucumberTest_SetUp.java
+++ cucumber-test/src/test/java/CucumberTest_SetUp.java
@@ -1,9 +1,10 @@
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/featureFiles/SetUp.feature", publish=true)
+@CucumberOptions(features = "src/test/resources/featureFiles/Setup")
 
 public class CucumberTest_SetUp {
 
