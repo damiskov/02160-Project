dac72698a1bfb58601e3ba6a5e060cbc34cc5176
David Miles-Skov <davidmilesskov@protonmail.com>
2022-03-23 15:10:02 +0100

    yabadabadoo

---
+++ cucumber-test/src/main/java/Board.java
@@ -0,0 +1,15 @@
+
+public class Board {
+
+	private Piece[][] matrix;
+	private int numObstacles;
+	public void setNumberOfObstacles(int n) {
+		// TODO Auto-generated method stub
+		this.numObstacles = n;
+		
+	}
+
+	
+	
+
+}
--- cucumber-test/src/test/java/StepsDefinitionDifficultySetup.java
+++ cucumber-test/src/test/java/StepsDefinitionDifficultySetup.java
@@ -3,25 +3,27 @@ import io.cucumber.java.en.Then;
 
 
 public class StepsDefinitionDifficultySetup {
+	
+	Board board = new Board();
+	Game game = new Game();
+	
+	
 	@Given("A Difficulty")
-	public void a_difficulty() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void a_difficulty(String d) {
+		game.setDifficulty(d);
 	}
+	
 	@Then("set number of obstacles")
-	public void set_number_of_obstacles() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void set_number_of_obstacles(int n) {
+	    board.setNumberOfObstacles(n);
 	}
 	@Then("set their location")
-	public void set_their_location() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void set_location(Piece p, int x, int y) {
+	    p.setPosition(x, y);
 	}
 	@Then("Generate board")
 	public void generate_board() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    game.genBoard();
 	}
 
 
