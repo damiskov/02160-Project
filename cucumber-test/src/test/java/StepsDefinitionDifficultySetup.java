79cf1938b78ba48b0f1ac1f0c84ae131936b7cb3
David Miles-Skov <davidmilesskov@protonmail.com>
2022-03-23 11:53:58 +0100

    Made step definitions for board generation and difficulty set up

---
+++ cucumber-test/src/test/java/StepsDefinitionDifficultySetup.java
@@ -0,0 +1,28 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+
+public class StepsDefinitionDifficultySetup {
+	@Given("A Difficulty")
+	public void a_difficulty() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("set number of obstacles")
+	public void set_number_of_obstacles() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("set their location")
+	public void set_their_location() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("Generate board")
+	public void generate_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
