6a31a70aac09782741e3654938c4a117403cdbd2
Ioannis Karras <101004898+s204725@users.noreply.github.com>
2022-03-23 15:48:36 +0100

    Added picocontainer dependency and Context class

---
+++ cucumber-test/src/test/java/Context.java
@@ -0,0 +1,16 @@
+import environment_elements.ConveyerBelt;
+import environment_elements.Gear;
+import environment_elements.HealthStation;
+import environment_elements.Pit;
+import environment_elements.Wall;
+import piece_basics.Robot;
+
+public class Context {
+//	IBoard board;
+	Robot robot;
+	ConveyerBelt conveyerBelt;
+	Gear gear;
+	HealthStation healthStation;
+	Pit pit;
+	Wall wall;
+}
