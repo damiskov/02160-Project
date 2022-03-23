8c16f20785a0c6dc3020b12cfa89dac71bb53905
Ioannis Karras <101004898+s204725@users.noreply.github.com>
2022-03-23 11:28:18 +0100

    Wrote step definitions for first four obstacles

---
+++ cucumber-test/src/test/java/StepDefinitionsGear.java
@@ -0,0 +1,37 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitionsGear {
+
+	@When("robot steps on a gear")
+	public void robot_steps_on_a_gear() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("a gear pointing towards right")
+	public void a_gear_pointing_towards_right() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("gear turns the robot towards right")
+	public void gear_turns_the_robot_towards_right() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("a gear pointing towards left")
+	public void a_gear_pointing_towards_left() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("gear turns the robot towards left")
+	public void gear_turns_the_robot_towards_left() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	
+	
+	
+}
