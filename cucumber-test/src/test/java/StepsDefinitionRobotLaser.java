89edde7ea79784c5a057c2b9355f431031ab99c3
aroojchaudhry <aroojchaudhry@aroojs-mbp.students.clients.local>
2022-03-23 11:37:15 +0100

    Three Interactions step definitions

---
+++ cucumber-test/src/test/java/StepsDefinitionRobotLaser.java
@@ -0,0 +1,266 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepsDefinitionRobotLaser {
+
+	//Successful upwards laser shot(Robot laser)
+	@Given("two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board column as B")
+	public void a_register_ends_with_a_in_the_same_board_column_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is below B")
+	public void a_is_below_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing up")
+	public void a_is_facing_up() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	//Successful downwards laser shot(Robot laser)
+	@Given("two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board column as B")
+	public void a_register_ends_with_a_in_the_same_board_column_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is above B")
+	public void a_is_above_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing down")
+	public void a_is_facing_down() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	//Successful rightwards laser shot(Robot laser)
+	@Given("Two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board row as B")
+	public void a_register_ends_with_a_in_the_same_board_row_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is to the left of B")
+	public void a_is_to_the_left_of_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing right")
+	public void a_is_facing_right() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	//Successful leftwards laser shot(Robot laser)
+	@Given("Two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board row as B")
+	public void a_register_ends_with_a_in_the_same_board_row_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is to the right of B")
+	public void a_is_to_the_right_of_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing left")
+	public void a_is_facing_left() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	//Blocked upwards laser shot(Robot laser)
+	@Given("two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("a blocking obstacle on the board")
+	public void a_blocking_obstacle_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board column as B")
+	public void a_register_ends_with_a_in_the_same_board_column_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is below B")
+	public void a_is_below_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing up")
+	public void a_is_facing_up() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("the blocking obstacle is between A and B")
+	public void the_blocking_obstacle_is_between_a_and_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	//Blocked downwards laser shot(Robot laser) 
+
+	@Given("two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("a blocking obstacle on the board")
+	public void a_blocking_obstacle_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board column as B")
+	public void a_register_ends_with_a_in_the_same_board_column_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is above B")
+	public void a_is_above_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing down")
+	public void a_is_facing_down() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("the blocking obstacle is between A and B")
+	public void the_blocking_obstacle_is_between_a_and_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	//Blocked rightwards laser shot(Robot laser)
+	@Given("Two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("a blocking obstacle on the board")
+	public void a_blocking_obstacle_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board row as B")
+	public void a_register_ends_with_a_in_the_same_board_row_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is to the left of B")
+	public void a_is_to_the_left_of_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing right")
+	public void a_is_facing_right() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("the blocking obstacle is between A and B")
+	public void the_blocking_obstacle_is_between_a_and_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	//Blocked leftwards laser shot(Robot laser) 
+	@Given("Two robots A and B on the board")
+	public void two_robots_a_and_b_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Given("a blocking obstacle on the board")
+	public void a_blocking_obstacle_on_the_board() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("a register ends with A in the same board row as B")
+	public void a_register_ends_with_a_in_the_same_board_row_as_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is to the right of B")
+	public void a_is_to_the_right_of_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("A is facing left")
+	public void a_is_facing_left() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("the blocking obstacle is between A and B")
+	public void the_blocking_obstacle_is_between_a_and_b() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("B takes damage")
+	public void b_takes_damage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	
+
+}
