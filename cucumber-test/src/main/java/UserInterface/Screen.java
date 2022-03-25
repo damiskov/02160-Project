a5422306f22ae1aa6eb62990fbbf3297e88ee050
BenceGattyan <bencegattyan@gmail.com>
2022-03-25 15:36:12 +0100

    Screen class added for UI package

---
+++ cucumber-test/src/main/java/UserInterface/Screen.java
@@ -0,0 +1,17 @@
+package UserInterface;
+
+import board.Board;
+
+public interface Screen {
+	
+	void update(Board board);
+	
+	Object output();
+
+	void setOutput(String string);
+
+	void displayObstacles();
+
+	
+
+}
