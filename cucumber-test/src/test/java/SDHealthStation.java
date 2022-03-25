46f8ebe69fbc7c8d9deef25f264d91414261c277
Ioannis Karras <101004898+s204725@users.noreply.github.com>
2022-03-25 14:38:41 +0100

    Wrote step definitions for HealthStation scenarios, and passed the tests
    Also added a heal() method to Robot which HealthStation calls

---
+++ cucumber-test/src/test/java/SDHealthStation.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+
+import board.Board;
+import environment_elements.HealthStation;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import piece_basics.Robot;
+
+public class SDHealthStation {
+	private Context context;
+	
+	public SDHealthStation(Context context) {
+		this.context = context;
+	}
+	
+	@Given("a health station on the board")
+	public void a_health_station_on_the_board() {
+	    Board board = context.board;
+	    HealthStation hs = new HealthStation(board, 6, 5);
+	    board.placeEElement(hs);
+	    context.healthStation = hs;
+	}
+	@Given("the robot does not have full health")
+	public void the_robot_does_not_have_full_health() {
+	    context.robot.takeDamage();
+	    context.robot.takeDamage();
+	}
+	@When("the robot moves into the health station during a register")
+	public void the_robot_moves_into_the_health_station_during_a_register() {
+		context.robot.shiftX(1);
+	}
+	@Then("the robot gains one HP at the end of the register")
+	public void the_robot_gains_one_hp_at_the_end_of_the_register() {
+	    Robot r = context.robot;
+		int oldHealth = r.getHealth();
+		context.healthStation.performRegisterAction();
+		assertEquals(oldHealth + 1, r.getHealth());
+	}
+	
+	@Given("the robot has full health")
+	public void the_robot_has_full_health() {}
+	@Then("the robot remains at full health at the end of the register")
+	public void the_robot_remains_at_full_health_at_the_end_of_the_register() {
+		Robot r = context.robot;
+		int oldHealth = r.getHealth();
+		context.healthStation.performRegisterAction();
+		assertEquals(oldHealth, r.getHealth());
+	}
+}
--- cucumber-test/src/test/resources/featureFiles/Interactions/Health station.feature
+++ cucumber-test/src/test/resources/featureFiles/Interactions/Health station.feature
@@ -1,14 +1,16 @@
 Feature: Health station
 
   Scenario: A damaged robot is healed by a health station
-    Given a robot on the board
+  	Given a board
+    And a robot on the board
     And a health station on the board
     And the robot does not have full health
     When the robot moves into the health station during a register
     Then the robot gains one HP at the end of the register
 
   Scenario: A robot is not healed by a health station, because it has full health
-    Given a robot on the board
+  	Given a board
+    And a robot on the board
     And a health station on the board
     And the robot has full health
     When the robot moves into the health station during a register
