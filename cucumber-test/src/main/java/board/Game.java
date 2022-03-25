df2efea558201f476379bee2e181bd84940c8a2f
David Miles-Skov <davidmilesskov@protonmail.com>
2022-03-25 16:11:09 +0100

    Changes to Game class and Player class. Created Deck class.
    Game class:
    - Added getter and setter for players in game.
    - Getter and setter for game difficulty
    - Added getCurrentPlayer method

--- cucumber-test/src/main/java/board/Game.java
+++ cucumber-test/src/main/java/board/Game.java
@@ -7,12 +7,62 @@ public class Game {
 	Board b;
 	String difficulty;
 	Player[] players;
+	Player currentPlayer;
+	Deck gameDeck;
+	
+	// Board methods
+	
 	public void genBoard() {
-		System.out.println("Joe");
+		if (difficulty.equals("easy"))
+		{
+			// Generate board corresponding to easy difficulty
+		}
+		if (difficulty.equals("medium"))
+		{
+			// Generate board corresponding to medium difficulty 
+		}
+		if (difficulty.equals("hard"))
+		{
+			// Generate board corresponding to hard difficulty
+		}
 	}
+	
+	// Difficulty getter and setters
+	
 	public void setDifficulty(String d) {
 		this.difficulty = d;
+	}
+	
+	public void getDifficulty()
+	{
+		System.out.println(difficulty);
+	}
+	
+	// Player methods
+	
+	public void setPlayers(Player[] p)
+	{
+		if (p.length >= 2 && p.length <= 8)
+		{
+			this.players = p;
+		}
+		else
+		{
+			System.out.println("Invalid number of players (Should be between 2 and 8 (inclusive))");
+		}
+	}
+	
+	public Player getCurrentPlayer()
+	{
+		return currentPlayer;
+	}
+	
+	public void displayBoard()
+	{
+		// Displaying board
 		
 	}
+	
+	
 
 }
