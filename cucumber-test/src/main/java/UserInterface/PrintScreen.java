f5f49845092ee57727bdad281f12a2ca23bf402f
BenceGattyan <bencegattyan@gmail.com>
2022-03-29 23:10:31 +0200

    Added PrintScreen class #2

---
+++ cucumber-test/src/main/java/UserInterface/PrintScreen.java
@@ -0,0 +1,61 @@
+package UserInterface;
+
+import board.Board;
+
+public class PrintScreen implements Screen{
+
+	@Override
+	public Object output() {
+		// TODO Auto-generated method stub huh
+		return null;
+	}
+
+	@Override
+	public void update(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOutput(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayObstacles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayRobots() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayRobotMarkers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayRobotHealth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showCurrPlayerHand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showCurrPlayerNumber() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
