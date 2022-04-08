f288b34e99e8837f0f951e8f7c42a5b470a6cb41
aroojchaudhury <aroojchaudhry1@gmail.com>
2022-04-08 17:02:29 +0200

    Cards - Completed all scenarios - Step definitions completed - Full coverage

--- cucumber-test/src/test/java/SDCard.java
+++ cucumber-test/src/test/java/SDCard.java
@@ -1,5 +1,5 @@
 import io.cucumber.java.en.*;
-
+import piece_basics.Orientation;
 import piece_basics.Robot;
 
 import static org.junit.Assert.assertEquals;
@@ -96,112 +96,177 @@ public class SDCard {
 	    assertEquals(context.board.getPosition(context.robot), new Position(5,6));
 	}
 
+	//scenario - Move the robot two steps in the direction it is facing
 	
-	@Given("a two steps card")
-	public void a_two_steps_card() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Given("a Move2 card")
+	public void a_Move2_card() {
+		context.card = new Move2();
+    
 	}
-	// @Given("a robot on the board")
+	@Given("a robot on the board3")
+	public void a_robot_on_the_board3() {
+		this.context.robot = new Robot();
+		this.context.board = new Board(12,12);
+		context.board.initialPlacement(context.robot, new Position(5,5));
+	}
 
-	// @When("the card is executed")
+	@When("the card is executed3")
+	public void the_card_is_executed3() {
+		context.card.executeAction(context.robot);
+		
+	
+	}
 
 	@Then("the robot moves two steps in its direction")
 	public void the_robot_moves_two_steps_in_its_direction() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertEquals(context.board.getPosition(context.robot), new Position(5,7));
 	}
-	
-	
-	
-	
-	@Given("three steps card")
-	public void three_steps_card() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-	// @Given("a robot on the board")
 
-	// @When("the card is executed")
+	
+	//scenario - Move the robot three steps in the direction it is facing
+	
+	@Given("a Move3 card")
+	public void a_Move3_card() {
+		context.card = new Move3();
+    
+	}
+	@Given("a robot on the board4")
+	public void a_robot_on_the_board4() {
+		this.context.robot = new Robot();
+		this.context.board = new Board(12,12);
+		context.board.initialPlacement(context.robot, new Position(5,5));
+	}
+
+	@When("the card is executed4")
+	public void the_card_is_executed4() {
+		context.card.executeAction(context.robot);
+		
+	
+	}
 
 	@Then("the robot moves three steps in its direction")
 	public void the_robot_moves_three_steps_in_its_direction() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertEquals(context.board.getPosition(context.robot), new Position(5,8));
 	}
 	
-	
 
+	//scenario - turn robot to the right
+	
 	@Given("Right Direction card")
 	public void right_direction_card() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		context.card = new TurnRight();
+	    
 	}
-	// @Given("a robot on the board")
+	
+	@Given("a robot on the board5")
+	public void a_robot_on_the_board5() {
+		this.context.robot = new Robot();
+		context.robot.setOrientation(Orientation.UP);
+		this.context.board = new Board(12,12);
+		context.board.initialPlacement(context.robot, new Position(5,5));
+	}
 
-	// @When("the card is executed")
+	@When("the card is executed5")
+	public void the_card_is_executed5() {
+		context.card.executeAction(context.robot);
+		
+	
+	}
 	
 	@Then("the robot moves ninety degrees to the right")
 	public void the_robot_moves_ninety_degrees_to_the_right() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertEquals(context.robot.getOrientation(), Orientation.RIGHT);
 	}
 	
 	
-	@Given("Left Direction card")
-	public void left_direction_card() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-	// @Given("a robot on the board")
+	//scenario - turn robot to the left
 	
-	// @When("the card is executed")
+	@Given("Left turn card")
+	public void left_turn_card() {
+		
+		context.card = new TurnLeft();
+	    
+	}
+	
+	@Given("a robot on the board6")
+	public void a_robot_on_the_board6() {
+		this.context.robot = new Robot();
+		context.robot.setOrientation(Orientation.UP);
+		this.context.board = new Board(12,12);
+		context.board.initialPlacement(context.robot, new Position(5,5));
+	}
+
+	@When("the card is executed6")
+	public void the_card_is_executed6() {
+		context.card.executeAction(context.robot);
+		
+	
+	}
 	
 	@Then("the robot moves ninety degrees to the left")
 	public void the_robot_moves_ninety_degrees_to_the_left() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertEquals(context.robot.getOrientation(), Orientation.LEFT);
+	}
+		
+	
+	
+	
+//scenario - turn robot to the right
+
+	@Given("UTurn card")
+	public void UTurn_card() {
+		
+		context.card = new UTurn();
+	    
 	}
 	
-	
-	
-	@Given("U-turn card")
-	public void u_turn_card() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-	// @Given("a robot on the board")
-	
-	// @When("the card is executed")
-	
-	@Then("the robot moves one hundred eighty degrees so it faces opposite direction")
-	public void the_robot_moves_one_hundred_eighty_degrees_so_it_faces_opposite_direction() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Given("a robot on the board7")
+	public void a_robot_on_the_board7() {
+		this.context.robot = new Robot();
+		context.robot.setOrientation(Orientation.UP);
+		this.context.board = new Board(12,12);
+		context.board.initialPlacement(context.robot, new Position(5,5));
 	}
 
+	@When("the card is executed7")
+	public void the_card_is_executed7() {
+		context.card.executeAction(context.robot);
+		
 	
+	}
+	
+	@Then("the robot faces opposite orientation")
+	public void the_robot_faces_opposite_orientation() {
+	    assertEquals(context.robot.getOrientation(), Orientation.DOWN);
+	}
+	
+
+	// Backup card 
 	
 	
 	@Given("Backup card")
 	public void backup_card() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		context.card = new BackUp();
 	}
-	// @Given("a robot on the board")
-
-	@When("the card is executed")
-	public void the_card_is_executed() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	
+	@Given("a robot on the board8")
+	public void a_robot_on_the_board8() {
+		this.context.robot = new Robot();
+		this.context.board = new Board(12,12);
+		context.board.initialPlacement(context.robot, new Position(5,5));
+	}
+	
+	
+	@When("the card is executed8")
+	public void the_card_is_executed8() {
+		context.card.executeAction(context.robot);
 	}
 	@Then("the robot moves one space back without changing its direction")
 	public void the_robot_moves_one_space_back_without_changing_its_direction() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertEquals(context.board.getPosition(context.robot), new Position(5,4));
 	}
-
-
+	
 
 
 }
--- cucumber-test/src/test/resources/featureFiles/Cards/Cards.feature
+++ cucumber-test/src/test/resources/featureFiles/Cards/Cards.feature
@@ -21,45 +21,45 @@ Feature: Programming Cards
     Then the robot moves one step in its direction
     
    
-  #Scenario: Move the robot two steps in the direction it is facing
-  #	Given a two steps card
-    #And a robot on the board
-    #When the card is executed
-    #Then the robot moves two steps in its direction
-    #
-  #Scenario: Move the robot three steps in the direction it is facing
-  #	Given three steps card
-    #And a robot on the board
-    #When the card is executed
-    #Then the robot moves three steps in its direction
-    #
-      #
-  #Scenario: Move the robot towards right at ninety degrees angle
-  #	Given Right Direction card
-    #And a robot on the board
-    #When the card is executed
-    #Then the robot moves ninety degrees to the right
-    #
-  #Scenario: Move the robot towards left at ninety degrees angle
-  #	Given Left Direction card
-    #And a robot on the board
-    #When the card is executed
-    #Then the robot moves ninety degrees to the left
-    #
-      #
-  #Scenario: Move the robot at one hundred eighty degrees angle
-  #	Given U-turn card
-    #And a robot on the board
-    #When the card is executed
-    #Then the robot moves one hundred eighty degrees so it faces opposite direction
-    #
-        #
-  #Scenario: Move the robot in the downwards direction
-  #	Given Backup card
-    #And a robot on the board
-    #When the card is executed
-    #Then the robot moves one space back without changing its direction
-    #
-#
-#
+  Scenario: Move the robot two steps in the direction it is facing
+  	Given a Move2 card
+    And a robot on the board3
+    When the card is executed3
+    Then the robot moves two steps in its direction
+    
+  Scenario: Move the robot three steps in the direction it is facing
+  	Given a Move3 card
+    And a robot on the board4
+    When the card is executed4
+    Then the robot moves three steps in its direction
+    
+      
+  Scenario: Rotate robot to the right through ninety degrees
+  	Given Right Direction card
+    And a robot on the board5
+    When the card is executed5
+    Then the robot moves ninety degrees to the right
+    
+    
+  Scenario: Rotate robot to the right through ninety degrees
+  	Given Left turn card
+    And a robot on the board6
+    When the card is executed6
+    Then the robot moves ninety degrees to the left 
+
+
+  Scenario: Move the robot at one hundred eighty degrees angle
+  	Given UTurn card
+    And a robot on the board7
+    When the card is executed7
+    Then the robot faces opposite orientation
+    
+    
+  Scenario: Move the robot in the downwards direction
+  	Given Backup card
+    And a robot on the board8
+    When the card is executed8
+    Then the robot moves one space back without changing its direction
+    
+
    
