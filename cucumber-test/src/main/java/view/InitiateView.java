d9fb653780425d97d653cdc2f785487f65523ab9
Patrick <patrick.j.martins@hotmail.com>
2022-04-08 14:37:40 +0200

    added difficulty to UI

--- cucumber-test/src/main/java/view/InitiateView.java
+++ cucumber-test/src/main/java/view/InitiateView.java
@@ -4,9 +4,12 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 
 import controller.InitiateController;
@@ -19,6 +22,11 @@ public class InitiateView extends JFrame{
 	
 	private JSlider sliderPlayers;
 	private JButton btnStart;
+	JPanel difContainer;
+	JRadioButton easyDifficulty;
+	JRadioButton mediumDifficulty;
+	JRadioButton hardDifficulty;
+	ButtonGroup difficulty;
 	private InitiateController controller;
 	
 	public InitiateView(InitiateController controller) {
@@ -29,13 +37,34 @@ public class InitiateView extends JFrame{
 	private void initGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		setTitle("Select number of players");
+		setTitle("Game setup");
 		setLayout(new GridBagLayout());
 		
 		sliderPlayers = new JSlider(2,8,2);
 		sliderPlayers.setMajorTickSpacing(1);
 		//sliderPlayers.setPaintTicks(true);
 		sliderPlayers.setPaintLabels(true);
+		
+		//Difficulty setup
+		difficulty = new ButtonGroup();
+		easyDifficulty = new JRadioButton("EASY");
+		mediumDifficulty = new JRadioButton("MEDIUM");
+		hardDifficulty = new JRadioButton("HARD");
+		
+		
+		
+		easyDifficulty.setSelected(true);
+		difficulty.add(easyDifficulty);
+		difficulty.add(mediumDifficulty);
+		difficulty.add(hardDifficulty);
+		
+		difContainer = new JPanel();
+		difContainer.add(easyDifficulty);
+		difContainer.add(mediumDifficulty);
+		difContainer.add(hardDifficulty);
+		
+		
+		
 		
 		btnStart = new JButton("Start Game");
 		btnStart.addActionListener(new ActionListener() {
@@ -47,7 +76,9 @@ public class InitiateView extends JFrame{
 		
 		add(new JLabel("No. of players:"), GridBagLayoutUtils.constraint(0, 0, 5));
 		add(sliderPlayers, GridBagLayoutUtils.constraint(1, 0, 5));
-		add(btnStart, GridBagLayoutUtils.constraint(1, 1, 5));
+		add(new JLabel("Choose a difficulty:"), GridBagLayoutUtils.constraint(0, 1, 5));
+		add(difContainer, GridBagLayoutUtils.constraint(1, 1, 5));
+		add(btnStart, GridBagLayoutUtils.constraint(1, 2, 5));
 		
 		pack();
 		setLocationRelativeTo(null);
