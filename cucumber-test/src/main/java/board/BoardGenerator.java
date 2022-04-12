4635881f3feba212f977134d36b9044bb69cc291
David Miles-Skov <davidmilesskov@protonmail.com>
2022-04-12 17:14:50 +0200

    More work on BoardGenerator class

--- cucumber-test/src/main/java/board/BoardGenerator.java
+++ cucumber-test/src/main/java/board/BoardGenerator.java
@@ -3,6 +3,7 @@ import java.util.Random;
 
 import environment_elements.*;
 import piece_basics.EnvironmentElement;
+import piece_basics.Orientation;
 import piece_basics.Robot;
 public class BoardGenerator {
 	
@@ -12,7 +13,15 @@ public class BoardGenerator {
 	{
 		b = new Board(12,12);
 		b.setNumberOfObstacles(numObstacles);
-		EnvironmentElement[] EElist = {new Gear(true), new Gear(false), };
+		EnvironmentElement[] EElist = {new Gear(true), new Gear(false),
+										new ConveyorBelt(Orientation.UP), new ConveyorBelt(Orientation.DOWN), new ConveyorBelt(Orientation.RIGHT), new ConveyorBelt(Orientation.LEFT),
+										new HealthStation(),
+										new Pit(),
+										new Wall(),
+										new ReversalPanel(),
+										new Laser(),
+										new OilSpill(),
+										new Teleporter()};
 		int[] startPositionsX = {5,6,4,7,3,7,2,1};
 		
 		// Initial placement of robots
@@ -32,9 +41,11 @@ public class BoardGenerator {
 			int x = rand.nextInt(12);
 			int y = 1 + rand.nextInt(11);
 			Position p = new Position(x,y);
+			// random obstacle
+			int o = rand.nextInt(EElist.length);
 			if (!b.hasEElementAt(p))
 			{
-				b.initialPlacement(, p);
+				
 			}
 			
 		}
