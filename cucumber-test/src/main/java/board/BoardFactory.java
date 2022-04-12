c375a6134b810532ab2eeea1c668e9c9d7fe7004
David Miles-Skov <davidmilesskov@protonmail.com>
2022-04-12 10:54:40 +0200

    No need for Board Generator class - deleted

--- cucumber-test/src/main/java/board/BoardFactory.java
+++ cucumber-test/src/main/java/board/BoardFactory.java
@@ -2,6 +2,20 @@ package board;
 
 public class BoardFactory {
 	
-	
+	public static Board generateBoard(Difficulty d)
+	{
+		if (d.getLevel()==1)
+		{
+			
+		} else if (d.getLevel()==2)
+		{
+			
+		} else if (d.getLevel()==3)
+		{
+			
+		}
+		
+		return null;
+	}
 
 }
