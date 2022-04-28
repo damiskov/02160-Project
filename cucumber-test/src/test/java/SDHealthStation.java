519771b4884d642dc8412fdda6cbecff804c5c84
Ioannis Karras <101004898+s204725@users.noreply.github.com>
2022-04-28 17:29:56 +0200

    Fixed HealthStation scenarios
    They used to call activateRegisterActors() in the Then step for some
    reason

--- cucumber-test/src/test/java/SDHealthStation.java
+++ cucumber-test/src/test/java/SDHealthStation.java
@@ -1,11 +1,5 @@
-import static org.junit.Assert.assertEquals;
-
-import board.Board;
 import environment_elements.HealthStation;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import piece_basics.Robot;
 
 public class SDHealthStation {
 	private Context context;
@@ -19,29 +13,5 @@ public class SDHealthStation {
 	    HealthStation hs = new HealthStation();
 	    context.board.initialPlacement(hs, int1, int2);
 	    context.healthStation = hs;
-	}
-	
-	@Given("the robot does not have full health")
-	public void the_robot_does_not_have_full_health() {
-	    context.robot.takeDamage();
-	    context.robot.takeDamage();
-	}
-	@Then("the robot gains one HP")
-	public void the_robot_gains_one_hp() {
-	    Robot r = context.robot;
-		int oldHealth = r.getHealth();
-		context.healthStation.performRegisterAction();
-		assertEquals(oldHealth + 1, r.getHealth());
-	}
-	
-	@Given("the robot has full health")
-	public void the_robot_has_full_health() {}
-	
-	@Then("the robot remains at full health")
-	public void the_robot_remains_at_full_health() {
-		Robot r = context.robot;
-		int oldHealth = r.getHealth();
-		context.healthStation.performRegisterAction();
-		assertEquals(oldHealth, r.getHealth());
 	}
 }
--- cucumber-test/src/test/resources/featureFiles/Interactions/Health station.feature
+++ cucumber-test/src/test/resources/featureFiles/Interactions/Health station.feature
@@ -3,15 +3,13 @@ Feature: Health station
   Scenario: A damaged robot is healed by a health station
   	Given a game with an empty board
   	And a health station on the board at (5, 5)
-  	And a robot on the board at (5, 5)
-  	And the robot does not have full health
+  	And a robot on the board at (5, 5) with 1 HP
     When the board elements activate
-    Then the robot gains one HP
+    Then the robot has 2 HP
 
   Scenario: A robot is not healed by a health station, because it has full health
   	Given a game with an empty board
   	And a health station on the board at (5, 5)
-  	And a robot on the board at (5, 5)
-    And the robot has full health
+  	And a robot on the board at (5, 5) with full health
     When the board elements activate
     Then the robot remains at full health
