22c59d2c94db227b4d0fc060696951298cd57d47
Ioannis Karras <101004898+s204725@users.noreply.github.com>
2022-04-30 17:05:28 +0200

    Comments in FinalCheckpoint, RespawnPoint, and Board, small changes

--- cucumber-test/src/main/java/environment_elements/FinalCheckpoint.java
+++ cucumber-test/src/main/java/environment_elements/FinalCheckpoint.java
@@ -12,6 +12,7 @@ public class FinalCheckpoint extends Checkpoint {
 	public void performImmediateAction(Robot r) {
 		super.performImmediateAction(r);
 		if (r.getMostRecentCheckpoint() == getNumber()) {
+			// Inform the Game that this robot won
 			board.getGame().finishGame(r.getRobotNumber());
 		}
 	}
--- cucumber-test/src/main/java/environment_elements/RespawnPoint.java
+++ cucumber-test/src/main/java/environment_elements/RespawnPoint.java
@@ -5,6 +5,8 @@ import piece_basics.EnvironmentElement;
 public class RespawnPoint extends EnvironmentElement {
 
 	public static final String ID = "respawn_point";
+	
+	// Respawning (rebooting) logic is handled in the Robot class
 
 	@Override
 	public String getPieceID() {
