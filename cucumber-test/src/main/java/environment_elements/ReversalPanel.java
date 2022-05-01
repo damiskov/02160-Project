0ec640eca06f81ea941d6910d05ca553129f35ac
LeoLeoLeoLeoLeoo <s204688@dtu.dk>
2022-05-01 15:17:00 +0200

    tried to fix stuff, failed, removed it

--- cucumber-test/src/main/java/environment_elements/ReversalPanel.java
+++ cucumber-test/src/main/java/environment_elements/ReversalPanel.java
@@ -29,53 +29,55 @@ public class ReversalPanel extends EnvironmentElement {
 			with the opposite card of each card in the program*/
 			System.out.println("-----------------------STEPPED ON REVERSAL PANEL---------------");
 			Card c = new Card("");
-			outer:
-			while (program.getCardList().size()!=0)
-			{
-				c = program.getTopOfProgram();
-				newCards.add(c.getOppositeCard());
-				System.out.println("Adding card"+c.getAction()+" to Program");
-				if(program.getCardList().size()==0)
-				{
-					System.out.println("End of program");
-					System.out.println("------------------END OF REVERSAL PANEL REGISTER ACTION-----");
-					break outer;
-				}
-
-			//Card c = new Card();
-			
-			
-//			Card c = program.getCardList().pop();
-//			while(program.getCardList().size() != 0) {
-//				System.out.println(program.getCardList().size());
-//				newProg.getCardList().add(c.getOppositeCard());
-//				c = program.getCardList().pop();
-//				if(program.getCardList().size() == 0) {
-//					newProg.getCardList().add(c.getOppositeCard());
+			//outer:
+//			while (program.getCardList().size()!=0)
+//			{
 //				
+//				c = program.getTopOfProgram();
+//				newCards.add(c.getOppositeCard());
+//				System.out.println("Adding card"+c.getAction()+" to Program");
+//				if(program.getCardList().size()==0)
+//				{
+//
+//					System.out.println("End of program");
+//					System.out.println("------------------END OF REVERSAL PANEL REGISTER ACTION-----");
+//					break outer;
 //				}
+//
+//			//Card c = new Card();
+//			
+//			
+////			Card c = program.getCardList().pop();
+////			while(program.getCardList().size() != 0) {
+////				System.out.println(program.getCardList().size());
+////				newProg.getCardList().add(c.getOppositeCard());
+////				c = program.getCardList().pop();
+////				if(program.getCardList().size() == 0) {
+////					newProg.getCardList().add(c.getOppositeCard());
+////				
+////				}
+////			}
+//				
+////				else {
+////					c = null;
+////				}
+//			
+//			
+//			
+//			System.out.println("-------------ASSIGNING NEW PROGRAM TO ROBOT ----------------");
+//			Program newProgram = new Program();
+//			Collections.reverse(newCards);
+//			newProgram.getCardList().addAll(newCards);
+//			for (Card i : newProgram.getCardList())
+//			{
+//				System.out.println("Program contains: " + i.getAction());
 //			}
-				
-//				else {
-//					c = null;
-//				}
-			
-			
-			
-			System.out.println("-------------ASSIGNING NEW PROGRAM TO ROBOT ----------------");
-			Program newProgram = new Program();
-			Collections.reverse(newCards);
-			newProgram.getCardList().addAll(newCards);
-			for (Card i : newProgram.getCardList())
-			{
-				System.out.println("Program contains: " + i.getAction());
-			}
-			
-			board.getRobotAt(calculatePosition()).setProgram(newProg.getCardList()); //set the robot's program to be the reversed program
-			System.out.println(board.getRobotAt(calculatePosition()).getProgram().getCardList());
-
-		}
-			System.out.println("------------- SUCCESSFULLY ADDED CARDS TO PROGRAM ------------");
+//			
+//			board.getRobotAt(calculatePosition()).setProgram(newProgram.getCardList()); //set the robot's program to be the reversed program
+//			System.out.println(board.getRobotAt(calculatePosition()).getProgram().getCardList());
+//
+//		}
+//			System.out.println("------------- SUCCESSFULLY ADDED CARDS TO PROGRAM ------------");
 		}
 
 	}
